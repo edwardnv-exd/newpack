22178cdf43a6974147c2abba3e08c16fe72f2e29
edwardnv-exd <edward_nv@exdion.com>
2024-06-06 15:16:12 +0530

    More fixex to newpack

--- build/lib/hello_package/__init__.py
+++
@@ -1,5 +0,0 @@
-from .hello import Hello
-
-def hello (name):
-    text = Hello (name)
-    text.say_hello ()
--- build/lib/hello_package/hello.py
+++
@@ -1,5 +0,0 @@
-class Hello (object):
-    def __init__ (self, name):
-        self.name = name
-    def say_hello (self):
-        print ("Hello " + str (self.name))
